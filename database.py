dddeb8ccfa06cc7af6e329435b942b0f523b516d
Bunyod16 <bunyodshams@gmail.com>
2022-02-14 20:24:41 +0800

    changed hirarchy

---
+++ database.py
@@ -0,0 +1,53 @@
+import csv 
+import os
+from google.cloud import firestore
+from utils import COLLECTION
+
+db = firestore.Client()
+
+def add_wallet(wallet, user_id, user_name, date):
+    user_id = str(user_id)
+    try:
+        db = firestore.Client()
+        doc_ref = db.collection(COLLECTION).document(user_id)
+        doc_ref.set({
+            "user_id" : user_id,
+            "user_name" : user_name,
+            "wallet": wallet,
+            "last_updated" : date
+        })
+    except:
+        return (0)
+    return (1)
+
+def get_wallet(user_id):
+    user_id = str(user_id)
+    doc_ref = db.collection(COLLECTION).document('237063450646282241')
+    doc = doc_ref.get()
+    if doc.exists:
+        return(doc.to_dict())
+    else:
+        return (0)
+
+def update_wallet(wallet_address, user_id, date):
+    user_id = str(user_id)
+    doc_ref = db.collection(COLLECTION).document('237063450646282241')
+    doc_ref.update({"wallet":wallet_address, "date":date})
+    return (1)   
+
+def db_export_csv():
+    users_ref = db.collection(COLLECTION)
+    docs = users_ref.stream()
+    ls = []
+    for doc in docs:
+        ls.append(doc.to_dict())
+
+    keys = ls[0].keys()
+    with open('whitelist.csv', 'w', newline='') as output_file:
+        dict_writer = csv.DictWriter(output_file, keys)
+        dict_writer.writeheader()
+        dict_writer.writerows(ls)
+
+def db_clear_csv():
+        os.system("rm whitelist.csv")
+
---
+++ main.py
@@ -0,0 +1,48 @@
+from flask import Flask, request, make_response, send_file
+from threading import Thread
+import database
+import os
+
+app = Flask('')
+@app.route('/')
+def main():
+    return "API DB IS LIVE!"
+
+@app.route("/api/add_wallet", methods=['POST'])
+def api_add_wallet():
+    args = dict(request.args)
+    response = database.add_wallet(args["wallet"], args["user_id"], args["user_name"], args["date"])
+    if (response == 0):
+        response = 400
+    else:
+        response = 200
+    return make_response("ok", response)
+
+@app.route("/api/get_wallet", methods=['GET'])
+def api_get_wallet():
+    args = dict(request.args)
+    response = database.get_wallet(args["user_id"])
+    if (not response):
+        return make_response("no wallet", 400)
+    return make_response(response,  200)
+
+@app.route("/api/update_wallet", methods=['POST'])
+def api_update_wallet():
+    args = dict(request.args)
+    response = database.update_wallet(args["wallet"], args["user_id"], args["date"])
+    if (response == 0):
+        return make_response("error", 400)
+    else:
+        return make_response("ok", 200)
+
+@app.route('/api/csv', methods=['GET'])
+def excel_export():
+    database.db_clear_csv()
+    database.db_export_csv()
+    try:
+        return send_file('./whitelist.csv', as_attachment=True)
+    except:
+        return make_response(404)
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
